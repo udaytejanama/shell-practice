0f6550ba80db5b8e6a6a1b95032fce18138c7457
uday teja <udayteja@gmail.com>
2025-09-22 12:35:33 +0530

    conversation commit

---
+++ 03.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON_1=Sachin
+PERSON_2=Rahul
+echo "$PERSON_1:: Hi $PERSON_2, How are you"
+echo "$PERSON_2:: Hi $PERSON_1, I am fine, How are you"
+echo "$PERSON_1:: I am good, what is your plan this weekend"
+echo "$PERSON_2:: Nothing $PERSON_1. We can plan if you are free"
