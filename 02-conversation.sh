f44e16a69cc7638dc102ee572ac739f86afc0629
uday teja <udayteja@gmail.com>
2025-09-22 12:28:53 +0530

    conversation commit

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi suresh, How are you"
+echo "Suresh:: Hi Ramesh, I am fine, How are you"
+echo "Ramesh:: I am good, what is your plan this weekend"
+echo "Suresh:: Nothing Ramesh. We can plan if you are free"
