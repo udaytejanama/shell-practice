bc488b21c04c19a2a139984b50ce8be0f9343476
uday teja <udayteja342@gmail.com>
2025-09-23 19:56:11 +0530

    args

---
+++ 12-installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Error:: Please run this script with root previlege"
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing the Mysql is a failure"
+    exit 1
+else
+    echo "Installing the Mysql is a failure"
+fi
+
