423ea0768b1a83c9bd4de4747c4caedcf3c5e26d
uday teja <udayteja342@gmail.com>
2025-09-23 16:48:27 +0530

    args

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON_1=$1
+PERSON_2=$2
+echo "$PERSON_1:: Hi $PERSON_2, Can we talk on Tarriffs and H1B"
+echo "$PERSON_2:: Hi $PERSON_1, Sure let us discuss and make some amendments"
+echo "$PERSON_1:: Thats nice to hear. We will meet in Newyork."
+echo "$PERSON_2:: Yes. I will make sure the arrangement are proper and ready."
