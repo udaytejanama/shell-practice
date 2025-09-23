77808697d546825c93e13f530fc047a2248c31ae
uday teja <udayteja342@gmail.com>
2025-09-23 18:06:36 +0530

    args

---
+++ 11.-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please enter any number"
+read -s NUMBER1
+
+if [ $NUMBER1 -lt 20 ]; then
+    echo "Given number: $NUMBER1 is less than 20"
+elif [ $NUMBER1 -eq 20 ]; then
+    echo "Given number : $NUMBER1 is equal to 20"
+else
+    echo "Given number : $NUMBER1 is greater than 20"
+fi
