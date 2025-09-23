381e6c63f90b9e019cfb4aef767955d4c22ca708
uday teja <udayteja342@gmail.com>
2025-09-23 20:14:17 +0530

    args

--- 14-colors.sh
+++ 14-colors.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-R = "\e[%31m"
-G = "\e[%32m"
-Y = "\e[%31m"
-N = "\e[%0m"
+R="\e[%31m"
+G="\e[%32m"
+Y="\e[%31m"
+N="\e[%0m"
 
 echo "Hello world $R COLOUR RED"
 echo "CHECK THIS COLOUR" 
