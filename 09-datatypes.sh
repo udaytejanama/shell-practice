638efa8bbe7da671db245f05d6fab5c01cbe51dd
uday teja <udayteja342@gmail.com>
2025-09-23 17:45:34 +0530

    args

--- 09-datatypes.sh
+++ 09-datatypes.sh
@@ -3,8 +3,8 @@
 NUMBER1=100.250
 NUMBER2=200.252
 NAME=DEVOPS
-sum=$(awk '{print $NUMBER1 + $NUMBER2}')
-echo "Total sum:: $sum"
+SUM=$(echo 100.250 200.252 | awk '{print $1 + $2}')
+echo "Total sum:: $SUM"
 TOTAL=$(($NUMBER1 + $NUMBER2 | bc))
 
 echo "Total sum:: $TOTAL"
