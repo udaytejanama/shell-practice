35955b7bc1573b4dc1ddf005747dda811bf0a45f
uday teja <udayteja342@gmail.com>
2025-09-23 20:13:22 +0530

    args

--- 13-functions.sh
+++ 13-functions.sh
@@ -3,11 +3,13 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "Please execute the script: $0 with root privilege"
+    exit 1
 fi
 
 VALIDATE (){
     if [ $1 -ne 0 ]; then
         echo "Installing $2 is a failure"
+        exit 1
     else 
         echo "Installing $2 is a success"
     fi
--- 14-colors.sh
+++ 14-colors.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-USERID=$(id -u)
-if [ $USERID -ne 0 ]; then
-    echo "Please execute the script: $0 with root privilege"
-fi
+R = "\e[%31m"
+G = "\e[%32m"
+Y = "\e[%31m"
+N = "\e[%0m"
 
-VALIDATE (){
-    if [ $1 -ne 0 ]; then
-        echo "Installing $2 is a failure"
-    else 
-        echo "Installing $2 is a success"
-    fi
-}
-
-dnf install mysql -y
-VALIDATE $? "Mysql"
-
-dnf install nginx  -y
-VALIDATE $? "Nginx"
-
-dnf install python3 -y
-VALIDATE $? "Python"
+echo "Hello world $R COLOUR RED"
+echo "CHECK THIS COLOUR" 
