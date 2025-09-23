84d14d6f5fd4a3cb9bf7138f729d5eeb402819c7
uday teja <udayteja342@gmail.com>
2025-09-23 17:17:44 +0530

    args

---
+++ 07-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+date=$(date)
+echo $date
+
+start_date=$(date +%s)
+echo $start_date
+
+sleep 10
+
+end_date=$(date %s)
+echo $end_date
+
+total_timediff=$((end_date - start_date))
+echo "total difference between start and end ::$total_timediff"
