1e28c3c2b273d4ef0bba1f51062b3c3972326ae3
uday teja <udayteja342@gmail.com>
2025-09-23 17:57:11 +0530

    args

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,7 +2,7 @@
 
 NUMBER1=10
 
-if [ $NUMER1 -lt 20]; then
+if [ $NUMBER1 -lt 20 ]; then
     echo "Given number: $NUMBER1 is less than 20"
 else
     echo "Given number greater than or equal to 20"
