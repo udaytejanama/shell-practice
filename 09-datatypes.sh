31f54e6ff34127becf8a3fb7065bb8cd7d597254
uday teja <udayteja342@gmail.com>
2025-09-23 17:49:29 +0530

    args

--- 09-datatypes.sh
+++ 09-datatypes.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-NUMBER1=100.250
-NUMBER2=200.252
+NUMBER1=100
+NUMBER2=200.
 NAME=DEVOPS
-SUM=$(echo $NUMBER1 $NUMBER2 | awk '{print $1 + $2}')
+
+SUM=$(echo $NUMBER1 $NUMBER2 | awk '{print $1 + $2}') #For decimal numbers
 echo "Total sum:: $SUM"
-TOTAL=$(($NUMBER1 + $NUMBER2 | bc))
+TOTAL=$(($NUMBER1 + $NUMBER2)) # For integers
 
 echo "Total sum:: $TOTAL"
+
+LEADERS=("MODI","PUTIN","TRUMP","XINPING")
+echo "The India leader: ${LEADERS[@0]}"
